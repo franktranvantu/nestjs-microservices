f428fa3206496cecc7e1e7462597a5fe70b2ed47
Frank Tran <dss_pm_se1@vtijs.com>
2021-04-06 00:35:51 +0700

    Update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { ProductModule } from "./product/product.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'frank',
+      password: 'frank',
+      database: 'nestjs-microservices',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    ProductModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
